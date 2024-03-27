03705c7a86473d012a36239ec95ee1335a0eb0f4
vaibhav <vaibhavmasetty06@gmail.com>
2024-03-27 07:39:59 -0400

    first commit

---
+++ TypesOfTriangle.java
@@ -0,0 +1,15 @@
+public class TypesOfTriangle {
+    public String triangleType(int[] nums) {
+        if(nums[0]+nums[1]>nums[2] && nums[1]+nums[2]>nums[0] && nums[2]+nums[0]>nums[1]){
+            if(nums[0]==nums[1] && nums[1]==nums[2]){
+                return "equilateral";
+            }else if(nums[0]==nums[1] || nums[1]==nums[2] || nums[2]==nums[0]){
+                return "isosceles";
+            }else{
+                return "scalene";
+            }
+        }else{
+            return "none";
+        }
+    }
+}
